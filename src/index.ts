e5e14f58cd952fbb950069b22136223b294de0ed
Takadenoshi <tasos@kadena.io>
2023-03-16 17:19:29 +0100

    note about assert.equals; success log at end of file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 import assert from 'assert';
 
+
 // write a function add(a, b) that returns the sum of numbers a and b
 assert.equal(add(1, 2), 3);
 assert.equal(add(3, 5), 8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if you are not familiar with assert.equals:
+// `assert.equals(first, second)` will succeed (silently) when first === second
+// if first !== second it will throw an error
+
+// code above here so that the following is printed when all assertions pass
+console.log('All assertions passed');
